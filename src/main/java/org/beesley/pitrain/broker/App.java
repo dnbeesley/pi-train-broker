d51e981e26c264de4cb23b23f5ad6bddea9d776b
Daniel Beesley <dan-n-b@hotmail.com>
2020-12-12 16:21:47 +0000

    Convert to WAR output.

--- src/main/java/org/beesley/pitrain/broker/App.java
+++ src/main/java/org/beesley/pitrain/broker/App.java
@@ -1,15 +1,12 @@
 package org.beesley.pitrain.broker;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("org.beesley.pitrain")
 @EntityScan("org.beesley.pitrain")
 @SpringBootApplication
-public class App {
-  public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
-  }
+public class App extends SpringBootServletInitializer {
 }
